4eff0f7d126e027e779dece2bdd3c502b8dc6d47
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-09-08 11:15:56 +0300

    Update link

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -4,7 +4,9 @@ const Header = ({ subtitle }) => {
   return (
     <header className="row">
       <div className="col-md-5">
-        <img src={logo} className="logo" alt="logo" />
+        <a href="/">
+          <img src={logo} className="logo" alt="logo" />
+        </a>
       </div>
       <div className="col-md-7 mt-5 subtitle">{subtitle}</div>
     </header>
