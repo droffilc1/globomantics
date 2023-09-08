65935dc7921506555e70fd28f318b5c0e8365baf
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-09-08 11:43:49 +0300

    Fix errors

--- src/search-results/search-results-row.js
+++ src/search-results/search-results-row.js
@@ -10,7 +10,7 @@ const SearchResultsRow = ({ house }) => {
   return (
     <tr onClick={setActive}>
       <td>{house.address}</td>
-      <td>{house.price}</td>
+      <td>${house.price}</td>
       <td>{house.likes}</td>
     </tr>
   );
